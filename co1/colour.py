4a61c902b07a0abfc1339dff4375f654dab01cf5
aswindev24 <150823891+aswindev24@users.noreply.github.com>
2023-11-23 11:47:55 +0530

    Delete co1 directory

--- co1/colour.py
+++
@@ -1,6 +0,0 @@
-colour=input("enter the colours with coma sperated")
-
-print("old colour are :",colour)
-newcolour=colour.split(",")
-x=len(newcolour)
-print("new colours are:",newcolour[0],newcolour[x-1])
--- co1/n.py
+++
@@ -1,6 +0,0 @@
-n=int(input("Enter a number :"))
-result=0
-for i in range(1,n+1):
-    print(n**i)
-    result=result+n**i
-print("sum=",result)
