dd3cc584471e44830d46de55f5ca2c4bd3383b4e
tombogy <116348341+tombogy@users.noreply.github.com>
2023-11-28 10:48:59 +0530

    Delete VARUN.PY directory

--- VARUN.PY/9.swapfirstandlastChar.py
+++
@@ -1,6 +0,0 @@
-string="python"
-x=list(string)
-temp=x[0]
-x[0]=x[-1]
-x[-1]=temp
-print("".join(x))
--- VARUN.PY/sumoflistelements.py
+++
@@ -1,3 +0,0 @@
-list=[11,22,33,44,55,66,77,88,99]
-total=sum(list)
-print("Sum of all elements in list:",total)
