979db0ec6bc9c7d6bd55a0a591dde2fab382b540
tombogy <116348341+tombogy@users.noreply.github.com>
2023-11-29 12:09:30 +0530

    itemlist.py

--- S1_MCA_05_AKASH.K/itemlist.py
+++ S1_MCA_05_AKASH.K/itemlist.py
@@ -1,1 +1,9 @@
+print('sum of  all items in a list ')
+
+
+list = [ 12,23,34,45,56]
+total = 0
+for i in range(0,len(list)):
+    total = total + list[i]
+print("sum of list elements are :",total)    
 
