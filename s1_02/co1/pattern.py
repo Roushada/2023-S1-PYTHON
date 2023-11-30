38729f6f1b466968f92609b8b79b9004aa1021e2
Abhinandabb <150114104+Abhinandabb@users.noreply.github.com>
2023-11-30 14:48:41 +0530

    Add files via upload

---
+++ s1_02/co1/pattern.py
@@ -0,0 +1,12 @@
+n=int(input("enter the number:"))
+for i in range(n):
+    for j in range(i+1):
+        print('*',end=' ')
+    print()
+
+for i in range(n):
+    for j in range(n,i+1,-1):
+        print('*',end=' ')
+    print()
+    
+    
