e4b68cad9ef9492672476dfad42c4452fc65ab55
nafiyakp <150060297+nafiyakp@users.noreply.github.com>
2023-12-03 20:26:49 +0530

    Update Q_3.py

--- S1_28 Nafiya.kp/CO1/Q_3.py
+++ S1_28 Nafiya.kp/CO1/Q_3.py
@@ -4,3 +4,17 @@ print(new)
 n=8
 list1=[x**2 for x in range(n+1)]
 print("the square of number is:",list1)
+#3)b
+n=8
+list1=[x**2 for x in range(n+1)]
+print("the square of number is:",list1)
+#3)c
+word="awhengclg"
+vowel="aeiouAEIOU"
+list=[x for x in word if x in vowel]
+print("vowels in the word is:",list)
+#3)d
+word="how are you"
+print("the enterd word is ",word)
+list1=[ord(value)for value in word]
+print("the ordinal values",list1)
