fa3fb3ed4165a6fa7ef50124aa55bee0dbc1eedc
Ajisaop <151126684+Ajisaop@users.noreply.github.com>
2023-12-06 19:54:17 +0530

    Add files via upload

---
+++ S1_MCA_04_AJISA.OP/CO1/CO3/pkg.py
@@ -0,0 +1,12 @@
+from college.collegedetails import collegeName
+from college.collegedetails import collegeDescription
+from college.department.mca import sem1
+from college.department.mca import sem3
+from college.department.cse import csedpt
+collegeName()
+collegeDescription()
+print("")
+sem1()
+sem3()
+print("")
+csedpt()
