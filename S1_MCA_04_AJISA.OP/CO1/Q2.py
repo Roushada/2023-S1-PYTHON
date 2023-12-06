a2e4e01b4f70331414ac87b8d03cf7727cdff162
Ajisaop <151126684+Ajisaop@users.noreply.github.com>
2023-12-06 20:11:32 +0530

    Delete S1_MCA_04_AJISA.OP/CO1/Q2.py

--- S1_MCA_04_AJISA.OP/CO1/Q2.py
+++
@@ -1,9 +0,0 @@
-current_year = int(input("Enter the current year:"))
-future_year = int(input("Enter the future year:"))
-if (current_year<future_year):
-    print("Leap years are")
-    for year in range(current_year,future_year+1):
-        if(year%4==0 and year%100!=0):
-            print(year)
-else:
-    print("no future Leap years found")
