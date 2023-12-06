f7b8ab46f74dc779ff0a75fb99240f9df9c3effe
FIDHAFEBINM <148366775+FIDHAFEBINM@users.noreply.github.com>
2023-12-06 13:13:28 +0530

    Q6.py

--- S-21-FIDHA FEBIN/CO1/Q6.py
+++ S-21-FIDHA FEBIN/CO1/Q6.py
@@ -1,4 +1,8 @@
-lst=["fidha","hiba","raniya","hanan"]
+lst=[]
+x=int(input("enter the length"))
+for i in range(x):
+    y=input("enter name")
+    lst.append(y)
 count=0
 for x in lst:
     count+=x.lower().count("a")
