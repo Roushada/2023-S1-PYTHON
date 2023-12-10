49e989d722cd5657179d5d139415288d4763b70b
Ajisaop <151126684+Ajisaop@users.noreply.github.com>
2023-12-10 09:14:10 -0800

    Add files via upload

---
+++ S1_MCA_04_AJISA.OP/CO1/CO2/CO2_Q3.py
@@ -0,0 +1,5 @@
+list=[3,5,7,8,9]
+sum=0
+for i in list:
+    sum=sum+i
+print("Sum of numbers:",sum)
---
+++ S1_MCA_04_AJISA.OP/CO1/CO2/CO2_Q4.py
@@ -0,0 +1,8 @@
+import math
+list=[]
+for n in range(1000,10000):
+    for j in range(32,100,1):
+       if i==j*j:
+           string=str(i)
+               if(int(string[1])%2==0 and int(string[1])%2==0 and int(string[3]%2==0:
+    print(string)
