85ccb4e75820da75e3e80f6bcaec836423920639
AyshaShahanaCK <150112024+AyshaShahanaCK@users.noreply.github.com>
2023-12-11 22:19:43 +0530

    Delete S1_15/Graphics directory

--- S1_15/Graphics/dGraphics/sphere.py
+++
@@ -1,7 +0,0 @@
-from math import *
-def sphereArea(r):
-    area=4*pi*r**2
-    print("The area of the sphere is",area)
-def spherePerimeter(r):
-    perimeter=2*pi*r
-    print("The perimeter of the sphere is",perimeter)
--- S1_15/Graphics/rectangle.py
+++
@@ -1,7 +0,0 @@
-def rectArea(l,b):
-    area=l*b
-    print("Area of the rectangle with length",l,"and breadth",b,"is",area)
-def rectPeri(l,b):
-    perimeter=2*(l+b)
-    print("Perimeter of the rectangle with length",l,"and breadth",b,"is",perimeter)
-    
