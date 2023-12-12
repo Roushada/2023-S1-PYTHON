c1c8a2de9126d7f24132f3bc58475ca4e8969252
AyshaShahanaCK <150112024+AyshaShahanaCK@users.noreply.github.com>
2023-12-12 15:10:58 +0530

    Add files via upload

---
+++ S1_15/liFromUser.py
@@ -0,0 +1,9 @@
+li = []
+n = int(input("Enter the list size "))
+
+#print("\n")
+for i in range(n):
+    print("Enter number at index", i )
+    item = int(input())
+    li.append(item)
+print("User list is ", li)
